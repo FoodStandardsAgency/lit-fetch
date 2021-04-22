b2b1a7f91f31c8e78dc3f7e8f244873759a3a048
Xavier gilbert <xavier.gilbert@food.gov.uk>
2021-04-22 14:29:38 +0100

    fix namespace dependencies in DESCRIPTION

--- R/app_server.R
+++ R/app_server.R
@@ -2,9 +2,10 @@
 #'
 #' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
+#'
 #' @import shiny
-#' @importFrom data.table copy
 #' @importFrom tibble tibble
+#' 
 #' @noRd
 app_server <- function(input, output, session) {
   
